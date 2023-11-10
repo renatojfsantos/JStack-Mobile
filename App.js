10eb0ec8fe460d3f7929ae9f39f7a0cfc7b6f254
Renato Santos <renato_jfs@hotmail.com>
2023-11-10 08:41:12 -0300

    SafeAreaView no iOS e no Android

--- App.js
+++ App.js
@@ -1,27 +1,42 @@
-import { ScrollView, StatusBar } from 'react-native';
+import { ScrollView, StatusBar, SafeAreaView } from 'react-native';
 
 import { Button } from './src/Button';
 
+console.log(StatusBar.currentHeight);
+
 export default function App() {
   return (
-    <ScrollView style={{ backgroundColor: '#222' }}>
-      <StatusBar
-        hidden={false}
-        // backgroundColor='#f00' // * Android only
-        backgroundColor="rgba(255, 0, 0, .5)" // * Android only
-        barStyle='light-content'
-        animated
-        translucent={true} // * Android only
-      />
+    // * SafeAreaView: iOS only
+    <SafeAreaView
+      style={{
+        marginTop: (
+          Platform.OS === 'android'
+            ? StatusBar.currentHeight
+            : 0
+        )
+      }}
+    >
+      <ScrollView
+        style={{ backgroundColor: '#222', height: '100%' }}
+      >
+        <StatusBar
+          hidden={false}
+          // backgroundColor='#f00' // * Android only
+          backgroundColor="rgba(255, 0, 0, .5)" // * Android only
+          barStyle='light-content'
+          animated
+          translucent={true} // * Android only
+        />
 
-      {/* <StatusBar
-        backgroundColor='#00f' // * Android only
-      /> */}
+        {/* <StatusBar
+          backgroundColor='#00f' // * Android only
+        /> */}
 
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </ScrollView>
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
